38190c40288bcfbc9a8fb77e5778c10b38373e9a
Khan Saad Bin Hasan <khansaadbinhasan@gmail.com>
2020-06-18 16:17:00 +0530

    Tested heading based code, made functions, added try catch block

--- src/Obstacle-Detection-and-Path-Planning-master/main2.py
+++ src/Obstacle-Detection-and-Path-Planning-master/main2.py
@@ -59,16 +59,16 @@ def make_connections():
     s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s1.connect((TCP_IP_RPI, TCP_PORT_WASD))
 
+    return s1
 
-    s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s2.bind((TCP_IP_WORKSTATION, TCP_PORT_IMU_DATA))
-    s2.listen(1)
+    # s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # s2.bind((TCP_IP_WORKSTATION, TCP_PORT_IMU_DATA))
+    # s2.listen(1)
 
-    conn, addr = s2.accept()
-    print ('Connection address:', addr)
+    # conn, addr = s2.accept()
+    # print ('Connection address:', addr)
 
-
-    return s1, conn
+    # return s1, conn
 
 def draw_source_dest(cap):
     # function to draw the source and destination
@@ -198,12 +198,47 @@ def get_cmd(cX, cY, min_v, tempx, tempy):
 
     return cmd
 
+def update_tracker_data(tracker, img):
+    success, bbox = tracker.update(img)
+
+    cX = 0
+    cY = 0
+ 
+    if success:
+        drawBox(img,bbox)
+        
+        # cX = int(bbox[0] + (bbox[2]/2))
+        # cY = int(bbox[1] + (bbox[3]/2))
+
+        cX = int((bbox[0] + bbox[2])/2)
+        cY = int((bbox[1] + bbox[3])/2)
+
+        cv2.putText(img,str(cX) + " " +str(cY), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    else:
+        cv2.putText(img, "Lost", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+ 
+    # cv2.rectangle(img,(15,15),(200,90),(255,0,255),2)
+
+    return cX, cY
 
 
-def finish(conn, s, cap, SEND_COMMAND):
+
+def make_grids(img, grid_size, winW, winH):
+    # this code is used for making grids
+    for (x, y, window) in process_image.sliding_window(img, stepSize=grid_size, windowSize=(winW, winH)):
+        # if the window does not meet our desired window size, ignore it
+        if window.shape[0] != winH or window.shape[1] != winW:
+            continue
+
+        cv2.rectangle(img, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+
+
+# def finish(conn, s, cap, SEND_COMMAND):
+def finish(s, cap, SEND_COMMAND):
     s.send(SEND_COMMAND.encode())
     s.close()
-    conn.close()
+    # conn.close()
     cap.release()
     cv2.destroyAllWindows()
 
@@ -232,7 +267,7 @@ def get_result(u,v):
 
     result = STOP
 
-    if angle_btw < minTheta and angle_btw > -minTheta:
+    if angle_btw > 180 - minTheta and angle_btw < 180 + minTheta:
         result = UP
 
     elif angle_btw < 180 - minTheta and angle_btw > minTheta:
@@ -241,7 +276,7 @@ def get_result(u,v):
     elif angle_btw > -(180 - minTheta) and angle_btw < -minTheta:
         result = RIGHT
 
-    elif angle_btw > 180 - minTheta and angle_btw < 180 + minTheta:
+    elif angle_btw < minTheta and angle_btw > -minTheta:
         result = DOWN
 
     else:
@@ -250,13 +285,59 @@ def get_result(u,v):
     return result
 
 
+def send_command(s, LAST_COMMAND, SEND_COMMAND):
+    if LAST_COMMAND != SEND_COMMAND:
+        s.send(SEND_COMMAND.encode())
+        LAST_COMMAND = SEND_COMMAND
+
+
+    return LAST_COMMAND
+
+
+def draw_circle_on_source(img, path):
+    if len(path):
+        for i in range(len(path)):
+            source = (path[i][0], path[i][1])
+
+            if i == 0 or i == len(path) - 1:
+                cv2.circle(img,source, 2, (255, 0, 0), 10)
+
+            else:
+                cv2.circle(img,source, 2, (255, 0, 0), 2)
+
+def run_tracker_algo(tracker, img, final_x, final_y, qt, grid_size, min_v):
+    global index, tempx, tempy
+
+    SEND_COMMAND = STOP
+
+    cX, cY = update_tracker_data(tracker, img)
+
+    #This function is not working correctly
+    if destination_reached(cX, cY ,final_x, final_y):
+        print("destination Reached")
+        return 'done'
+
+
+    if index < len(qt) - 1:
+        # assume vehicle did not stop at any of the subgoal
+        if distance(tempx, tempy ,cX, cY) < grid_size:
+            index += 1
+            min_v = 500
+
+        tempx, tempy = qt[index]
+        tempx, tempy = tempx*grid_size, tempy*grid_size
+
+        draw_pos_info(img, cX, cY, tempx, tempy)
+        SEND_COMMAND = get_cmd(cX, cY, min_v, tempx, tempy)
+
+    return SEND_COMMAND
 
 
 def main():
     SEND_COMMAND = STOP
     LAST_COMMAND = SEND_COMMAND
 
-    # s, conn = make_connections()
+    s = make_connections()
 
     cap = cv2.VideoCapture(numCam)
 
@@ -298,40 +379,23 @@ def main():
 
         # accel, gyro, mag, temp = get_IMU_data(conn)
 
-        # print("accel: ", accel)
-        # print("gyro: ", gyro)
-        # print("mag: ", mag)
-        # print("temp: ", temp)
-
-
-        # timer = cv2.getTickCount()
-        # success, img = cap.read()
-        # img = cv2.resize(img,(frame_width, frame_height))
-        # success, bbox = tracker.update(img)
-     
-        # if success:
-        #     drawBox(img,bbox)
-            
-        #     cX = int(bbox[0] + (bbox[2]/2))
-        #     cY = int(bbox[1] + (bbox[3]/2))
-
-        #     cv2.putText(img,str(cX) + " " +str(cY), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-
-        # else:
-        #     cv2.putText(img, "Lost", (100, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-     
-        # cv2.rectangle(img,(15,15),(200,90),(255,0,255),2)
-
-        # for drawing the matrix on to the frame
-        
+        timer = cv2.getTickCount()
         _, img = cap.read()
         img = cv2.resize(img,(frame_width, frame_height))
 
-        x1, y1, ret1 = rst.get_moments(img, o1, o2, 1)
-        x2, y2, ret2 = rst.get_moments(img, p1, p2, 2)
+        x1, y1 = 0, 0
+        x2, y2 = 0, 0
+
+        try:
+            # for drawing the matrix on to the frame
+            x1, y1, _ = rst.get_moments(img, o1, o2, 1)
+            x2, y2, _ = rst.get_moments(img, p1, p2, 2)
+
+        except:
+            pass
 
         xt = (x1+x2)//2
-        yt = (y1 +y2)//2
+        yt = (y1+y2)//2
 
         cv2.circle(img,(int(xt), int(yt)), 2, (0, 255, 0), 5)
 
@@ -341,33 +405,34 @@ def main():
             print("destination Reached")
             break
 
-        # if distance(xt, yt ,final_x, final_y)<grid_size:
-        #     print("destination Reached")
-
-        # car_angle=rst.get_direction(x1, y1, x2, y2)
-        # actual_angle = rst.get_direction(next_x, next_y, xt, yt)
-
-        # car_angle = 180 - car_angle
-        # actual_angle = 180 - actual_angle
-
-        # result=rst.path_encode(car_angle, actual_angle)
-
         u = ( x2 - x1, y2 - y1 )
         v = ( xt - next_x, yt - next_y )
-        result = get_result(u,v)
-        print(result)
+        SEND_COMMAND = get_result(u,v)
+
+        LAST_COMMAND = send_command(s, LAST_COMMAND, SEND_COMMAND)
 
         img = cv2.line(img,(x1, y1),(x2, y2), (0,255,0), 3)
         img = cv2.line(img, (xt, yt),(next_x, next_y),(0,0,255), 3)
 
 
-        if len(path):
-            for i in range(len(path)):
-                source = (path[i][0], path[i][1])
-                if i==0 or i==len(path)-1:
-                    cv2.circle(img,source, 2, (255, 0, 0), 10)
-                else:
-                    cv2.circle(img,source, 2, (255, 0, 0), 2) 
+        draw_circle_on_source(img, path)
+        make_grids(img, grid_size, winW, winH)
+
+        img = cv2.polylines(img, [pts] , False, (255,120,255), 3)
+
+        # # SEND_COMMAND = run_tracker_algo(tracker, img, final_x, final_y, qt, grid_size, min_v)
+
+
+        # if SEND_COMMAND == 'done':
+        #     break
+
+        # to print the direcction of the car
+        print('Action ' + direction[SEND_COMMAND])
+        
+
+        # LAST_COMMAND = send_command(s, LAST_COMMAND, SEND_COMMAND)
+        # draw_circle_on_source(img, path)
+ 
         
         cv2.imshow('window', img)
 
@@ -375,64 +440,8 @@ def main():
             break 
 
 
-
-
-        # # this code is used for making grids
-        # for (x, y, window) in process_image.sliding_window(img, stepSize=grid_size, windowSize=(winW, winH)):
-        #     # if the window does not meet our desired window size, ignore it
-        #     if window.shape[0] != winH or window.shape[1] != winW:
-        #         continue
-
-        #     cv2.rectangle(img, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-
-     
-        # fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-        # cv2.putText(img,str(int(fps)), (75, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (20,20,230), 2);
-     
-        # img = cv2.polylines(img, [pts] , False, (255,120,255), 3)
-
-
-        # #This function is not working correctly
-        # if destination_reached(cX, cY ,final_x, final_y):
-        #     print("destination Reached")
-        #     break
-
-
-        # if index < len(qt) - 1:
-        #     # assume vehicle did not stop at any of the subgoal
-        #     if distance(tempx, tempy ,cX, cY) < grid_size:
-        #         print('next point')
-        #         index += 1
-        #         min_v = 500
-
-        #     tempx, tempy = qt[index]
-        #     tempx, tempy = tempx*grid_size, tempy*grid_size
-
-        #     draw_pos_info(img, cX, cY, tempx, tempy)
-        #     SEND_COMMAND = get_cmd(cX, cY, min_v, tempx, tempy)
-
-        # # to print the direcction of the car
-        # print('Action ' + direction[SEND_COMMAND])
-     
-        # if LAST_COMMAND != SEND_COMMAND:# or timeElapsed % 3 == 0 :
-        #     # s.send(SEND_COMMAND.encode())
-        #     LAST_COMMAND = SEND_COMMAND
-
-        # if len(path):
-        #     for i in range(len(path)):
-        #         source = (path[i][0], path[i][1])
-        #         if i==0 or i==len(path)-1:
-        #             cv2.circle(img,source, 2, (255, 0, 0), 10)
-        #         else:
-        #             cv2.circle(img,source, 2, (255, 0, 0), 2) 
-        
-        # cv2.imshow('window', img)
-
-        # if cv2.waitKey(2) & 0xFF == 27:
-        #     break 
-
-
     SEND_COMMAND = STOP
+    finish(s, cap, SEND_COMMAND)
     # finish(conn, s, cap, SEND_COMMAND)
 
 if __name__ == '__main__':
